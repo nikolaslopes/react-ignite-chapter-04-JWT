1f0ae26cb75f568f735cdca8019748a22e060700
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-07-10 23:30:28 -0400

    :fire: created user interface

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,11 +1,17 @@
 import { AxiosError } from 'axios'
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 import { Api } from '../services/Api'
-import { AuthContextData, IAuthProvider, ISignInCredentials } from './types'
+import {
+  AuthContextData,
+  IAuthProvider,
+  ISignInCredentials,
+  IUser,
+} from './types'
 
 export const AuthContext = createContext({} as AuthContextData)
 
 export const AuthProvider = ({ children }: IAuthProvider) => {
+  const [user, setUser] = useState<IUser>()
   const isAuthenticated = false
 
   async function signIn({ email, password }: ISignInCredentials) {
--- src/context/types.ts
+++ src/context/types.ts
@@ -4,11 +4,16 @@ export interface AuthContextData {
   signIn(credentials: ISignInCredentials): Promise<void>
   isAuthenticated: boolean
 }
+export interface IAuthProvider {
+  children: ReactNode
+}
 export interface ISignInCredentials {
   email: string
   password: string
 }
 
-export interface IAuthProvider {
-  children: ReactNode
+export interface IUser {
+  email: string
+  permissions: string[]
+  roles: string[]
 }
