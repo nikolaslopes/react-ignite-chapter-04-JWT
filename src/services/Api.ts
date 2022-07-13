f7f8cf0716ac0ac24dca13cd597aa1a82d3df25c
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-07-13 15:15:14 -0400

    :fire: add interceptors

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,11 +1,29 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { parseCookies } from 'nookies'
 
-const { NEXT_AUTH_BASE_TOKEN: token } = parseCookies()
+export interface IAxiosErrorResponse {
+  code?: string
+}
+
+let cookies = parseCookies()
 
 export const Api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${cookies['NEXT_AUTH_BASE_TOKEN']}`,
   },
 })
+
+Api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error: AxiosError<IAxiosErrorResponse>) => {
+    if (error.response?.status === 401) {
+      if (error.response?.data?.code === 'token.expired') {
+        cookies = parseCookies()
+      } else {
+      }
+    }
+  }
+)
